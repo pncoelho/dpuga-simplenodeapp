29f36de3b3bb282dfacbdc982e88e3b3e9de3f90
pncoelho <pncoelho@outlook.pt>
2022-05-15 23:41:54 +0000

    commit to run both workflows

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-# Comment to trigger workflow
+# Comment to trigger both workflow
 
